3b1fcb7421d9db2d33cff358dbaa11e5b1d272eb
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2025-07-28 20:34:26 +0530

    added sql solution

---
+++ 01-select/02_find_customer_referee.sql
@@ -0,0 +1,7 @@
+-- LeetCode Problem: 584.Find Customer Referee
+-- Question: https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2 OR referee_id  IS NULL
