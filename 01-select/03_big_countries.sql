c14181ee52f5fd74bf60881c021fa7aa764dfb13
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2025-07-28 20:51:14 +0530

    added sql solution

---
+++ 01-select/03_big_countries.sql
@@ -0,0 +1,7 @@
+-- LeetCode Problem:  595. Big Countries
+-- Question: https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+SELECT name, population, area
+FROM World
+WHERE area >=3000000 OR population >=25000000;
